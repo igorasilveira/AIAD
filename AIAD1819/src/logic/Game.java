c85a19a20d28c44c893c0d3242545030d67429ad
rmcarvalho <rmcarvalho@users.noreply.github.com>
2018-10-10 08:48:32 +0100

    created Game class
    Co-Authored-By: leonardogomesc <leonardogomesc@users.noreply.github.com>

---
+++ AIAD1819/src/logic/Game.java
@@ -0,0 +1,14 @@
+package logic;
+
+public class Game {
+
+	public Game() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
