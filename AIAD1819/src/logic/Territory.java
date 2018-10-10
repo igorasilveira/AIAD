41979eb5a24101c0a05c7ae1a6dd3595820ccfb0
rmcarvalho <rmcarvalho@users.noreply.github.com>
2018-10-10 09:54:51 +0100

    added Territory class, image of game board and updated README
    Co-Authored-By: leonardogomesc <leonardogomesc@users.noreply.github.com>

---
+++ AIAD1819/src/logic/Territory.java
@@ -0,0 +1,17 @@
+package logic;
+
+import java.util.ArrayList;
+
+public class Territory {
+
+	/**
+	 * List of all adjacent or connected territories
+	 */
+	private ArrayList<Territory> neighbours;
+	
+	
+	public Territory() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
