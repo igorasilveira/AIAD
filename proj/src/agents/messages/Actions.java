804c1ff4ae6bd2acc69ac4bb8760e70bed1353ee
rmcarvalho <rmcarvalho@users.noreply.github.com>
2018-11-06 15:36:25 +0000

    refactored attack class and added actions

--- proj/src/agents/messages/Actions.java
+++ proj/src/agents/messages/Actions.java
@@ -1,5 +1,17 @@
 package agents.messages;
-
+/**
+ * Possible actions depend on the needed action
+ * 
+ * Setup - request player to begins his turn by placing a certain amount of pieces
+ * Play - request player to make a play 
+ * Attack - propose an attack
+ * Fortify - propose a fortification
+ * Done - propose no action
+ * Defend - request player to defnd himself
+ * Treaty - propose a treaty to another player
+ * 
+ */
 public enum Actions {
-	Play, Attack, Fortify, Done, Defend, Treaty
+	Setup, Play, Attack, Fortify, Done, Defend, Treaty
 }
+
--- proj/src/logic/Attack.java
+++ proj/src/logic/Attack.java
@@ -6,8 +6,11 @@ public class Attack implements Serializable {
 	public final Territory attacker;
 	public final Territory defender;
 	
-	public Attack(Territory attacker, Territory defender) {
+	public final int diceAmount;
+	
+	public Attack(Territory attacker, Territory defender, int diceAmount) {
 		this.attacker = attacker;
 		this.defender= defender;
+		this.diceAmount = diceAmount;
 	}
 }
